716fbf908a068c9c7a6c3aa178d39b4a9d8760be
Solomon Rajan <compactor_relative296@slmails.com>
2025-09-03 21:26:40 +0530

    fix: update author name in site configuration

--- src/site.config.ts
+++ src/site.config.ts
@@ -5,7 +5,7 @@ export const theme: ThemeUserConfig = {
   /** Title for your website. Will be used in metadata and as browser tab title. */
   title: 'Astro Theme Pure',
   /** Will be used in index page & copyright declaration */
-  author: 'Pure Lab',
+  author: 'Solomon Rajan',
   /** Description metadata for your website. Can be used in page metadata. */
   description: 'Stay hungry, stay foolish',
   /** The default favicon for your site which should be a path to an image in the `public/` directory. */
